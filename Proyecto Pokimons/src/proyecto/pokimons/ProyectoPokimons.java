bf33b580a7f04dffdb329d35ee66d12632fb1eb3
alumno informatica <alumno@ciis.usil.edu.pe>
2019-05-27 12:06:09 -0500

    Version Beta del proyecto

---
+++ Proyecto Pokimons/src/proyecto/pokimons/ProyectoPokimons.java
@@ -0,0 +1,12 @@
+package proyecto.pokimons;
+import Interfaces.*;
+public class ProyectoPokimons {
+
+    public static void main(String[] args) {
+        
+        jfMenuPrincipal mp = new jfMenuPrincipal();
+        mp.setVisible(true);
+        
+    }
+    
+}
